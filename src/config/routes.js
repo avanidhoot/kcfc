0453ec9fb0a5af2615f1af66cd536f3c3f9b36b7
Revansiddh Patil <rpatil1@DSRM.local>
2018-07-03 12:53:43 +0530

    project setup routes and layout.

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Switch, Route } from 'react-router'
 import { Container } from 'semantic-ui-react';
 import { Sidebar } from '../component';
