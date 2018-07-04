4bb47df55e2270a8223c29ad92e2d65dc133a0ec
Ganesh Khedkar <gkhedkar@DSRM.local>
2018-07-04 17:27:40 +0530

    add header

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,7 +9,8 @@ import CR from "../module/CR";
 const Routes = ()=> {
     return(        
         <Router>
-            <Container style={{height:'100vh'}}>
+            <Container style={{height:'100vh'}}>            
+                <NewComponent/>
                 <Sidebar>                        
                     <Route exact path="/" component={CR} />
                     <Route path="/eri" component={ERI} />
@@ -18,6 +19,37 @@ const Routes = ()=> {
             </Container>
         </Router>
     )
+}
+const  NewComponent = ()=>{
+      return (
+        <center>
+          <div style={{width: 950, height: 80}}>
+            <div style={{float: 'left'}}>
+              <img src={require('../images/KATALYSTTECH-LO-FF.jpg')} alt="Logo" style={{border: '6px solid #c3c3c3', borderRadius: 4, WebkitBorderRadius: 4, MozBorderRadius: 4}} /></div>
+            <div style={{float: 'right'}}>
+              <img src={require('../images/KSSPL logo.png')} alt="Logo" style={{border: '6px solid #c3c3c3', borderRadius: 4, WebkitBorderRadius: 4, MozBorderRadius: 4, height: 65, backgroundColor: '#ffffff'}} /></div>
+            <div style={{float: 'right', width: 480, textAlign: 'right', paddingRight: 10}}>
+              <p>
+                Representation letter for all submissions</p>
+              <p>
+                Rate confirmation/contingent offer letter</p>
+              <p>
+                For US Hires please CC to <a href="mailto:?cc=katalysthr@katalysttech.com">katalysthr@katalysttech.com</a>
+                on all offer emails.</p>
+              <p>
+                For India Hires please CC to <a href="mailto:?cc=katalystindiahr@katalysttech.com">katalystindiahr@katalysttech.com</a>
+                on all offer emails.</p>
+            </div>
+          </div>
+          <hr style={{clear: 'both', width: 950}} />
+          <div style={{width: 950}}>
+            <div style={{textAlign: 'right', lineHeight: '1.5em'}}><a href="http://192.168.1.206/Documents/AdditionalInfo/ChecklistFormatsforSalesteam.xlsx">Recruiting Checklist</a><br />
+              <a href="http://192.168.1.206/Documents/AdditionalInfo/RecruitingStaffing_CheetSheet.docx">Recruiting &amp; Staffing Cheat Sheet</a>
+            </div>
+            <div style={{float: 'none'}} />
+          </div>
+        </center>
+      );
 }
 
 const App = (props) =>{
