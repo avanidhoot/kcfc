2f62f226048ad0edacb20843953a1a3776ea23bc
Ganesh Khedkar <gkhedkar@DSRM.local>
2018-07-05 11:18:54 +0530

    optimization  the code

--- src/module/ERI/index.js
+++ src/module/ERI/index.js
@@ -21,47 +21,12 @@ export default class ERI extends Component {
   }
 
   render() {
-    const { activeIndex } = this.state;
-    const listItem = eriData.ERI.map(m=>{
-        return <ul>
-                <List.Item>
-                    <li>  
-                        <a target="_blank" href={m.link}>
-                            {m.title}
-                        </a> 
-                    </li>                   
-                </List.Item>
-            </ul>
-    });
-        
-    const listItem2 = eriData.ForKatalyst.map(m=>{
-        return <ul>
-                <List.Item>
-                    <li> 
-                        <a target="_blank" href={m.link}>
-                            {m.title}
-                        </a>
-                    </li>          
-                </List.Item>
-            </ul>        
-    })
-
-    const listItem3 = eriData.ForKSSPL.map(m=>{
-        return <ul>
-            <List.Item>
-                <li>
-                    <a target="_blank" href={m.link}>
-                        {m.title}
-                    </a>     
-                </li>               
-            </List.Item>
-        </ul>
-    })    
+    const { activeIndex } = this.state;   
     return (
          <Accordion styled>
-            <Accord index={0} activeIndex={activeIndex} title="Employee Relevant Information" item={listItem} handleClick={this.handleClick} />
-            <Accord index={1} activeIndex={activeIndex} title="For Katalyst" item={listItem2} handleClick={this.handleClick} />
-            <Accord index={2} activeIndex={activeIndex} title="For KSSPL" item={listItem3} handleClick={this.handleClick} />
+            <Accord index={0} activeIndex={activeIndex} title="Employee Relevant Information" item={eriData.ERI} handleClick={this.handleClick} />
+            <Accord index={1} activeIndex={activeIndex} title="For Katalyst" item={eriData.ForKatalyst} handleClick={this.handleClick} />
+            <Accord index={2} activeIndex={activeIndex} title="For KSSPL" item={eriData.ForKSSPL} handleClick={this.handleClick} /> 
         </Accordion>     
     )
   }
@@ -71,13 +36,23 @@ const Accord = (props) =>{
     return(
         <React.Fragment>
             <Accordion.Title active={props.activeIndex.includes(props.index)} index={props.index} onClick={(e,titleProps)=>props.handleClick(e,titleProps)}>
-                    <Icon name='dropdown' />
-                    {props.title}
+                <Icon name='dropdown' />
+                {props.title} 
             </Accordion.Title>
             <Accordion.Content active={props.activeIndex.includes(props.index)}>
-                    <p>
-                    {props.item}
-                    </p>
+                <p>
+                   { props.item.map(m=>{ 
+                    return  <ul key={m.id}>
+                        <List.Item>
+                            <li>
+                                <a target="_blank" href={m.link}>
+                                    {m.title}
+                                </a>     
+                            </li>               
+                        </List.Item>
+                    </ul>
+                    })}
+                </p>
             </Accordion.Content>
         </React.Fragment>
     )
